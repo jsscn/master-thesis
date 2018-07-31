6d733023b0e3c8341e3f350c3a0f6431280802c4
Josse Coen <josse.coen@gmail.com>
2018-07-31 14:54:39 +0200

    More writing, notably on cohesion
    Update latexify.py to read JSON files instead of some random format

--- latexify.py
+++ latexify.py
@@ -1,26 +1,35 @@
 import sys
+import json
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-c", type=str, default="parallel") # parallel or serial (parallel by default, I guess)
-parser.add_argument("-f", type=str)
-parser.add_argument("-o", type=str)
+parser.add_argument("-c", default="parallel") # parallel or serial (parallel by default, I guess)
+parser.add_argument("-f")
+parser.add_argument("-o")
+parser.add_argument("-l")
 parser.add_argument("--math-mode-delimiters", action="store_true")
 parser.add_argument("--regular-text", action="store_true")
-parser.add_argument("--frequencies", action="store_true")
+parser.add_argument('--no-frequencies', action='store_true')
 
 args = parser.parse_args()
 
+if args.l:
+    with open(args.l) as f:
+        labels = f.read().splitlines()
+        label = lambda event_type: labels[event_type] if event_type < len(labels) else str(event_type)
+else:
+    label = lambda event_type: str(event_type)
 
 f = sys.stdin if args.f is None else open(args.f)
 out = sys.stdout if args.o is None else open(args.o, "w")
 
-for line in f:
-    if ":" in line: continue
+j = json.load(f)
 
-    elements = line.split()[:-1]
-    frequency = float(line.split()[-1][1:-1])
+for episode in j['episodes']:
+
+    elements = [label(e) for e in episode['event-types']]
+    frequency = episode.get('frequency')
 
     elements_it = ("\\text{{{}}}".format(e) for e in elements) if args.regular_text \
         else elements
@@ -33,7 +42,7 @@ for line in f:
     if args.math_mode_delimiters:
         latexified = "$ {} $".format(latexified)
 
-    if args.frequencies:
+    if not args.no_frequencies and frequency is not None:
         latexified = "{} ({:.3g})".format(latexified, frequency)
 
     print(latexified, file=out)
