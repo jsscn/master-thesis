9ee0b346b5da01691ff84d1c641183e2c8452aa6
Josse Coen <josse.coen@gmail.com>
2018-08-04 16:14:36 +0200

    Stuff towards quality experiments

--- latexify.py
+++ latexify.py
@@ -28,17 +28,28 @@ def latexify_episode(episode, args):
     return latexified
 
 def latexify(thing, args):
+    if (args.fci or args.qcsp) and hasattr(thing, 'read'):
+        lines = thing.read().splitlines()
+        latexifieds = []
+        for line in lines:
+            split = line.split()
+            if args.fci:
+                pattern = [item.replace('_', '\\_') for item in split[:-1]]
+                (frequency, length, cohesion) = split[-1].split(';')[1:-1]
+            else:
+                pattern = split[0][1:-1].replace('_', '\\_').split(',')
+                cohesion = split[2].replace(',', '.')
+
+            latexifieds.append(latexify({'event-types': pattern, 'frequency': float(cohesion)}, args))
+        return latexifieds
+
     if isinstance(thing, dict) and 'event-types' in thing:
         latexified = latexify_episode(thing, args)
 
         return latexified if latexified is not None else None
 
     if isinstance(thing, dict):
-        new_thing = {}
-        # new_thing = {key : latexify(item, args) for key, item in thing.items()}
-        for key, item in thing.items():
-            new_thing[key] = latexify(item, args)
-        return new_thing
+        return  {key : latexify(item, args) for key, item in thing.items()}
 
     if isinstance(thing, list):
         return [latexify(item, args) for item in thing]
@@ -53,6 +64,8 @@ parser.add_argument("-l")
 parser.add_argument("--math-mode-delimiters", action="store_true")
 parser.add_argument("--regular-text", action="store_true")
 parser.add_argument('--no-frequencies', action='store_true')
+parser.add_argument('--fci', action='store_true')
+parser.add_argument('--qcsp', action='store_true')
 
 args = parser.parse_args()
 
@@ -66,17 +79,21 @@ else:
 f = sys.stdin if args.f is None else open(args.f)
 out = sys.stdout if args.o is None else open(args.o, "w")
 
-j = json.load(f)
+if args.fci or args.qcsp:
+    latexified = list(latexify(f, args))
+    print('\n'.join(str(e) for e in latexified), file=out)
+else:
+    j = json.load(f)
 
-latexified = latexify(j, args)
+    latexified = latexify(j, args)
 
-print('Global top k:', file=out)
-print('\n'.join(latexified['top-k-episodes']['global']), file=out)
-print('Per-size top k:', file=out)
-print('\n'.join('{}-episodes:\n{}'.format(
-    i, '\n'.join(str(e) for e in i_episodes)) for i, i_episodes in enumerate(latexified['top-k-episodes']['per-size'], start=1)), file=out)
-print('All episodes:', file=out)
-print('\n'.join(latexified['episodes']), file=out)
+    print('Global top k:', file=out)
+    print('\n'.join(latexified['top-k-episodes']['global']), file=out)
+    print('Per-size top k:', file=out)
+    print('\n'.join('{}-episodes:\n{}'.format(
+        i, '\n'.join(str(e) for e in i_episodes)) for i, i_episodes in enumerate(latexified['top-k-episodes']['per-size'], start=1)), file=out)
+    print('All episodes:', file=out)
+    print('\n'.join(latexified['episodes']), file=out)
 
 
 f.close()
